3d2690e41a94704bd4664421b888eee8a85a1d5d
Jimena <jimenaespinoza@gmail.com>
2025-01-30 13:33:41 -0300

    Updating of the CRUD in clientController file, to update and delete clients

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -3,9 +3,9 @@ const { createArticle, getArticles, getArticlesById, updateArticleById, deleteAr
 
 const ArticlesRoutes = express.Router();
 
-ArticlesRoutes.get('/', getArticles); //localhost:3003/api/articles/
-ArticlesRoutes.get('/:id', getArticlesById); //localhost:3003/api/articles/:id
 ArticlesRoutes.post('/create', createArticle);  //localhost:3003/api/articles/create
+ArticlesRoutes.get('/readall', getArticles); //localhost:3003/api/articles/readall
+ArticlesRoutes.get('/readone/:id', getArticlesById); //localhost:3003/api/articles/readone/:id
 ArticlesRoutes.put('/update-articles/:id', updateArticleById); //localhost:3003/api/articles/update-articles/:id
 ArticlesRoutes.delete('/delete-articles/:id', deleteArticleById); //localhost:3003/api/articles/delete-articles/:id
 
