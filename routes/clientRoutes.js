b2d5bba9db0878d246ff03632f8aa6a3b24af511
Jimena <jimenaespinoza@gmail.com>
2025-02-02 12:47:44 -0300

    Changes were made to the updating routes

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -6,7 +6,7 @@ const clientRoutes = express.Router();
 clientRoutes.get('/', getAllClients); //localhost:3003/api/clients/
 clientRoutes.delete('/:id', deleteClientById); //localhost:3003/api/clients/:id
 
-clientRoutes.put('/update', updateClientById); //localhost:3003/api/clients/update/:id
+clientRoutes.put('/update/:id', updateClientById); //localhost:3003/api/clients/update/:id
 clientRoutes.post('/register', ClientRegistration);  //localhost:3003/api/clients/register
 clientRoutes.post('/client-login', clientLogin); //localhost:3003/api/clients/client-login
 clientRoutes.get('/verify-client', auth, ClientVerification); //localhost:3003/api/clients/verify-client
